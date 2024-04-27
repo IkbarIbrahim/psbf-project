767edebaf566ae13433eceb7c2d7ddbc98fc346d
ahmadNoval <149749575+Vall-Here@users.noreply.github.com>
2024-04-27 13:16:11 +0700

    update

--- resources/views/Layout/Master.blade.php
+++ resources/views/Layout/Master.blade.php
@@ -1,9 +1,11 @@
-<!doctype html>
-<html class="w-full">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <header class="text-gray-600 body-font">
@@ -27,7 +29,7 @@
           </button>
         </div>
       </header>
+
+      @yield('content')
 </body>
 </html>
-
-
