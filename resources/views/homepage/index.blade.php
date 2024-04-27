ff7163a25e3834e9e4d9f3e57a74ccc11cdcf0e9
ahmadNoval <149749575+Vall-Here@users.noreply.github.com>
2024-04-27 14:32:48 +0700

    update

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -1,4 +1,13 @@
 @extends('layout.master')
 @section('title','Project || HOME PAGE')
 @section('content')
+@section('header-title','Home Page')
 
+
+
+<section class="w-ful px-20 h-full">
+    <h1 class="text-5xl">Ubah link ke /Admin</h1>
+</section>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homepage.index');
 });
+
+Route::get('/Admin', function () {
+    return view('Admin.index');
+});
+
